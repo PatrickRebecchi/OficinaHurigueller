6137ee539f9bcd3453dba4489efec188b3dbe851
Patrick <patrickrebecchi@hotmail.com>
2024-08-26 19:59:28 -0300

    Criando Pasta Models e classe Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OficinaHurigueller.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Placa { get; set; }
+        public string ModeloCarro { get; set; }
+        public string Telefone { get; set; }
+    }
+}
