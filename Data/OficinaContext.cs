3a11fbb3ad5ada8fb40f7721f87fd57fd888edc3
Patrick <patrickrebecchi@hotmail.com>
2024-08-26 20:01:10 -0300

    Criando a pasta Data e o DbContext

---
+++ Data/OficinaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OficinaHurigueller.Models;
+
+namespace OficinaHurigueller.Data
+{
+    public class OficinaContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=OficinaHurigueller.db");
+        }
+    }
+}
